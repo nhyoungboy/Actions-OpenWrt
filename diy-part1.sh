ca35516eed46b8da9c11d99bfea42c07057a127d
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2024-05-14 20:15:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #src-git small8 https://github.com/kenzok8/openwrt-packages
 
-cat >> feeds.conf.default <<EOF
-src-git kenzo https://github.com/kenzok8/openwrt-packages
-src-git small https://github.com/kenzok8/small
-EOF
+ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
